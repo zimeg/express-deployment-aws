078d6f6750c95aaf0f639f41692dbd7fa158ed37
Ethan Zimbelman <itszimbo@gmail.com>
2021-02-28 20:39:25 -0800

    added URL param ability

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const ApiBuilder = require('claudia-api-builder');
 
 const api = new ApiBuilder();
 
-api.get('/hello', () => 'hello world');
+api.get('/hello', (req) => (
+  `hello ${req.queryString.name ? req.queryString.name : 'world'}`
+));
 
 module.exports = api;
