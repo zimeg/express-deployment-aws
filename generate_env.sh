fc7c254d04ed2e136c379f1c779c35e52324de3a
Ethan Zimbelman <itszimbo@gmail.com>
2021-03-22 15:04:40 -0700

    add env vars to backend

--- generate_env.sh
+++
@@ -1,4 +0,0 @@
-# https://stackoverflow.com/questions/25378013/how-to-convert-a-json-object-to-key-value-format-in-jq
-for keyval in $(grep -E '": [^\{]' | sed -e 's/: /=/' -e "s/\(\,\)$//"); do
-    echo "$keyval"
-done
